f6ec274b11f6a56fcce4f95e5c661be257d32f40
ckjavahelpmate <ck.javahelpmate@gmail.com>
2025-12-16 17:34:29 +0530

    Even.js updated

--- C09_Events.js
+++ C09_Events.js
@@ -1,14 +1,17 @@
 /*
 Event in JS
 -----------------------------
--> the change in the state of an object is known as Event
+-> The change in the state of an object is known as Event
 -> Example :
         1. Mouse events( click, double click, hover)
         2. Keyboard events( keypress )
         3. Form events( form submit)
         4. Print event etc...
 
-
+-> Event-related properties
+   -> element.onclick = function () { ... };
+   -> element.onchange = function () { ... };
+   -> element.oninput = function () { ... };
 
 
 
